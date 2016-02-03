687098b294930dc23c8671a6980b4ffe10442c71
ctdk <jbingham@gmail.com>
2016-02-03 15:29:08 -0800

    Revert "reverted back to the frontendsetting.go from master"
    This reverts commit 1f0f14da18ab7c53f22d4d295446fe3c332a3274.

--- pkg/api/frontendsettings.go
+++ pkg/api/frontendsettings.go
@@ -6,7 +6,7 @@ import (
 	"github.com/grafana/grafana/pkg/bus"
 	"github.com/grafana/grafana/pkg/log"
 	"github.com/grafana/grafana/pkg/middleware"
-	m "github.com/grafana/grafana/pkg/models"
+	"github.com/grafana/grafana/pkg/models"
 	"github.com/grafana/grafana/pkg/plugins"
 	"github.com/grafana/grafana/pkg/setting"
 	"github.com/grafana/grafana/pkg/util"
@@ -130,6 +130,7 @@ func getFrontendSettingsMap(c *middleware.Context) (map[string]interface{}, erro
 			"panels": map[string]interface{}{
 				"raintankCallToAction": map[string]string{"path": "../plugins/raintank/panels/raintankCallToAction", "name": "Raintank Call To Action"},
 				"raintankEventsPanel":  map[string]string{"path": "../plugins/raintank/panels/raintankEventsPanel", "name": "Raintank Events"},
+				"raintankEndpointNavPanel":  map[string]string{"path": "../plugins/raintank/panels/raintankEndpointNavPanel", "name": "Raintank Nav Panel"},				
 				"raintankEndpointList": map[string]string{"path": "../plugins/raintank/panels/raintankEndpointList", "name": "Raintank Endpoint List"},
 			},
 		},
