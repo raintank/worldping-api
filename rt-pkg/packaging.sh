343b7c67ce89073787b0261fe68e1bdece0720fb
Anthony Woods <awoods@raintank.io>
2016-05-24 05:38:14 +0800

    convert to using git tag for package versioning

--- rt-pkg/packaging.sh
+++ rt-pkg/packaging.sh
@@ -5,15 +5,12 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 
 NAME=worldping-api
-VERSION="0.0.2" # need an automatic way to do this again :-/
+VERSION=$(git describe --long)
 BUILD="${DIR}/${NAME}-${VERSION}"
 ARCH="$(uname -m)"
-PACKAGE_NAME="${DIR}/artifacts/${NAME}-VERSION-ITERATION_ARCH.deb"
+PACKAGE_NAME="${DIR}/artifacts/${NAME}-${VERSION}_${ARCH}.deb"
 GOBIN="${DIR}/.."
-ITERATION=`date +%s`ubuntu1
-TAG="pkg-${VERSION}-${ITERATION}"
 
-git tag $TAG
 
 mkdir -p ${BUILD}/usr/share/${NAME}
 mkdir -p ${BUILD}/etc/init
@@ -26,6 +23,6 @@ cp ${DIR}/artifacts/bin/worldping-api ${BUILD}/usr/sbin/
 cp ${DIR}/artifacts/conf/sample.ini ${BUILD}/etc/raintank/worldping-api.ini
 
 fpm -s dir -t deb \
-  -v ${VERSION} -n ${NAME} -a ${ARCH} --iteration $ITERATION --description "Worldping Backend service" \
+  -v ${VERSION} -n ${NAME} -a ${ARCH} --description "Worldping Backend service" \
   --deb-upstart ${DIR}/conf/worldping-api \
   -C ${BUILD} -p ${PACKAGE_NAME} .
