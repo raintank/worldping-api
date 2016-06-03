527786105d0f7be4977d51ac701d55b07003c9fb
woodsaj <anthony@monkey.id.au>
2016-06-04 04:25:51 +0800

    add unit tests for v2Quotas api and v1 MonitorSettings

--- pkg/api/v1_test.go
+++ pkg/api/v1_test.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/Unknwon/macaron"
 	"github.com/go-xorm/xorm"
+	"github.com/raintank/worldping-api/pkg/api/rbody"
 	m "github.com/raintank/worldping-api/pkg/models"
 	"github.com/raintank/worldping-api/pkg/services/sqlstore"
 	"github.com/raintank/worldping-api/pkg/services/sqlstore/sqlutil"
@@ -37,13 +38,25 @@ func addAuthHeader(req *http.Request) {
 	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", setting.AdminKey))
 }
 
-func TestV1Api(t *testing.T) {
+func TestQuotasV1Api(t *testing.T) {
 	InitTestDB(t)
 	r := macaron.Classic()
 	setting.AdminKey = "test"
+	setting.Quota = setting.QuotaSettings{
+		Enabled: false,
+		Org: &setting.OrgQuota{
+			Endpoint: 10,
+			Probe:    10,
+		},
+		Global: &setting.GlobalQuota{
+			Endpoint: -1,
+			Probe:    -1,
+		},
+	}
 	Register(r)
 
 	Convey("When quotas not enabled", t, func() {
+		setting.Quota.Enabled = false
 		Convey("Given GET request for /api/org/quotas", func() {
 			resp := httptest.NewRecorder()
 			req, err := http.NewRequest("GET", "/api/org/quotas", nil)
@@ -73,17 +86,7 @@ func TestV1Api(t *testing.T) {
 	endpointCount := 0
 	probeCount := 0
 	Convey("When quotas are enabled", t, func() {
-		setting.Quota = setting.QuotaSettings{
-			Enabled: true,
-			Org: &setting.OrgQuota{
-				Endpoint: 10,
-				Probe:    10,
-			},
-			Global: &setting.GlobalQuota{
-				Endpoint: -1,
-				Probe:    -1,
-			},
-		}
+		setting.Quota.Enabled = true
 		Convey("Given GET request for /api/org/quotas", func() {
 			resp := httptest.NewRecorder()
 			req, err := http.NewRequest("GET", "/api/org/quotas", nil)
@@ -140,5 +143,159 @@ func TestV1Api(t *testing.T) {
 			})
 		})
 	})
+}
 
+func TestQuotasV2Api(t *testing.T) {
+	InitTestDB(t)
+	r := macaron.Classic()
+	setting.AdminKey = "test"
+	setting.Quota = setting.QuotaSettings{
+		Enabled: false,
+		Org: &setting.OrgQuota{
+			Endpoint: 10,
+			Probe:    10,
+		},
+		Global: &setting.GlobalQuota{
+			Endpoint: -1,
+			Probe:    -1,
+		},
+	}
+	Register(r)
+
+	Convey("When quotas not enabled", t, func() {
+		setting.Quota.Enabled = false
+		Convey("Given GET request for /api/v2/quotas", func() {
+			resp := httptest.NewRecorder()
+			req, err := http.NewRequest("GET", "/api/v2/quotas", nil)
+			So(err, ShouldBeNil)
+			addAuthHeader(req)
+
+			r.ServeHTTP(resp, req)
+			Convey("should return 200", func() {
+				So(resp.Code, ShouldEqual, 200)
+				Convey("quota response should be valid ApiResponse", func() {
+					response := rbody.ApiResponse{}
+					err := json.Unmarshal(resp.Body.Bytes(), &response)
+					So(err, ShouldBeNil)
+					So(response.Meta.Code, ShouldEqual, 200)
+					So(response.Meta.Type, ShouldEqual, "quotas")
+
+					quota := make([]m.OrgQuotaDTO, 0)
+					err = json.Unmarshal(response.Body, &quota)
+					So(err, ShouldBeNil)
+
+					So(len(quota), ShouldEqual, 2)
+					So(quota[0].OrgId, ShouldEqual, 1)
+					So(quota[0].Limit, ShouldEqual, -1)
+					So(quota[0].Used, ShouldEqual, -1)
+					for _, q := range quota {
+						So(q.Target, ShouldBeIn, "endpoint", "probe")
+					}
+				})
+			})
+		})
+	})
+
+	endpointCount := 0
+	probeCount := 0
+	Convey("When quotas are enabled", t, func() {
+		setting.Quota.Enabled = true
+		Convey("Given GET request for /api/org/quotas", func() {
+			resp := httptest.NewRecorder()
+			req, err := http.NewRequest("GET", "/api/v2/quotas", nil)
+			So(err, ShouldBeNil)
+			addAuthHeader(req)
+
+			r.ServeHTTP(resp, req)
+			Convey("should return 200", func() {
+				So(resp.Code, ShouldEqual, 200)
+				Convey("quota response should be valid", func() {
+					response := rbody.ApiResponse{}
+					err := json.Unmarshal(resp.Body.Bytes(), &response)
+					So(err, ShouldBeNil)
+					So(response.Meta.Code, ShouldEqual, 200)
+					So(response.Meta.Type, ShouldEqual, "quotas")
+					quota := make([]m.OrgQuotaDTO, 0)
+					err = json.Unmarshal(response.Body, &quota)
+					So(err, ShouldBeNil)
+
+					So(len(quota), ShouldEqual, 2)
+
+					for i, _ := range []int{1, 2, 3} {
+						Convey(fmt.Sprintf("when %d endpoints", i), func() {
+							err := sqlstore.AddEndpoint(&m.EndpointDTO{
+								Name:  fmt.Sprintf("test%d", i),
+								OrgId: 1,
+							})
+							endpointCount = i
+							So(err, ShouldBeNil)
+							for _, q := range quota {
+								So(quota[0].OrgId, ShouldEqual, 1)
+								So(quota[0].Limit, ShouldEqual, 10)
+								So(q.Target, ShouldBeIn, "endpoint", "probe")
+								if q.Target == "endpoint" {
+									So(q.Used, ShouldEqual, endpointCount)
+								}
+							}
+						})
+					}
+					for i, _ := range []int{1, 2, 3} {
+						Convey(fmt.Sprintf("when %d probes", i), func() {
+							err := sqlstore.AddProbe(&m.ProbeDTO{
+								Name:  fmt.Sprintf("test%d", i),
+								OrgId: 1,
+							})
+							probeCount = i
+							So(err, ShouldBeNil)
+							for _, q := range quota {
+								So(quota[0].OrgId, ShouldEqual, 1)
+								So(quota[0].Limit, ShouldEqual, 10)
+								So(q.Target, ShouldBeIn, "endpoint", "probe")
+								if q.Target == "probe" {
+									So(q.Used, ShouldEqual, probeCount)
+								}
+							}
+						})
+					}
+				})
+			})
+		})
+	})
+}
+
+func TestMonitorTypesV1Api(t *testing.T) {
+	r := macaron.Classic()
+	setting.AdminKey = "test"
+	Register(r)
+
+	Convey("Given GET request for /api/monitor_types", t, func() {
+		resp := httptest.NewRecorder()
+		req, err := http.NewRequest("GET", "/api/monitor_types", nil)
+		So(err, ShouldBeNil)
+		addAuthHeader(req)
+
+		r.ServeHTTP(resp, req)
+		Convey("should return 200", func() {
+			So(resp.Code, ShouldEqual, 200)
+			Convey("monitor_types response should be valid", func() {
+				monitorTypes := make([]m.MonitorTypeDTO, 0)
+				err := json.Unmarshal(resp.Body.Bytes(), &monitorTypes)
+				So(err, ShouldBeNil)
+				So(len(monitorTypes), ShouldEqual, 4)
+				for _, mType := range monitorTypes {
+					So(mType.Name, ShouldBeIn, "HTTP", "HTTPS", "Ping", "DNS")
+					switch mType.Name {
+					case "HTTP":
+						So(len(mType.Settings), ShouldEqual, 7)
+					case "HTTPS":
+						So(len(mType.Settings), ShouldEqual, 8)
+					case "Ping":
+						So(len(mType.Settings), ShouldEqual, 2)
+					case "DNS":
+						So(len(mType.Settings), ShouldEqual, 6)
+					}
+				}
+			})
+		})
+	})
 }
