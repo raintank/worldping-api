2f99919f309645beda7a818bf11f388abcac6a3d
woodsaj <anthony@monkey.id.au>
2016-08-22 16:32:58 +0800

    deploy docker image to dockerhub

---
+++ scripts/deploy_docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -x
+# Find the directory we exist within
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd ${DIR}
+
+VERSION=`git describe --always`
+
+docker push raintank/worldping-api:$VERSION
+docker push raintank/worldping-api:latest
